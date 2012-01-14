73529576b41b96bda9bd78bc38b621b422a3f0dd
greenboxal <jonathan_2097@hotmail.com>
2012-01-14 05:32:58 -0200

    GRF reading algorithm improved and save support added, acts like a repack(need to test but i think that is ok).

---
+++ GRFSharper/SaveGrfTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SaveGrfTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SAIB.SharpGRF.SharpGRF grf = new SAIB.SharpGRF.SharpGRF("test.grf");
+            grf.AddNewFile("test.xml", new byte[16854]);
+            grf.Save();
+        }
+    }
+}
